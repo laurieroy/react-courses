60a95310fcb106754bfeef980c762cd9478059c2
Laurie Roy <laurieroy.dev@gmail.com>
2022-02-07 15:19:47 -0800

    Handle server side errors

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -58,10 +58,15 @@ function ManageCoursePage({
     e.preventDefault();
 
     setSaving(true);
-    saveCourse(course).then(() => {
-      toast.success("Course saved.");
-      history.push("/courses");
-    });
+    saveCourse(course)
+      .then(() => {
+        toast.success("Course saved.");
+        history.push("/courses");
+      })
+      .catch((error) => {
+        setSaving(false);
+        setErrors({ onSave: error.message });
+      });
   }
 
   return authors.length === 0 || courses.length === 0 ? (
