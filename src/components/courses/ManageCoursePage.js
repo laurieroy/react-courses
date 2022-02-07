35898c0b1c5d16ca4e8da6090da2ccafe2b64f2c
Laurie Roy <laurieroy.dev@gmail.com>
2022-02-06 17:47:56 -0800

    Convert to functional

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,33 +1,30 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
-import { loadAuthors } from "../../redux/actions/authorActions"; 
-import { loadCourses } from "../../redux/actions/courseActions"; 
+import { loadAuthors } from "../../redux/actions/authorActions";
+import { loadCourses } from "../../redux/actions/courseActions";
 
-class ManageCoursePage extends Component {
-  componentDidMount() {
-    const { courses, authors, loadAuthors, loadCourses } = this.props;
-
+function ManageCoursePage({ courses, authors, loadAuthors, loadCourses }) {
+  useEffect(() => {
     if (authors.length === 0) {
       loadAuthors().catch((error) => {
         alert("Loading authors failed" + error);
       });
     }
+
     if (courses.length === 0) {
       loadCourses().catch((error) => {
         alert("Loading courses failed" + error);
       });
     }
-  }
+  }, []);
 
-  render() {
-    return (
-      <>
-        <h2>Manage Course</h2>
-      </>
-    );
-  }
+  return (
+    <>
+      <h2>Manage Course</h2>
+    </>
+  );
 }
 
 ManageCoursePage.propTypes = {
@@ -46,7 +43,7 @@ function mapStateToProps(state) {
 
 const mapDispatchToProps = {
   loadAuthors,
-  loadCourses
+  loadCourses,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
